9c156f229b138e8db0eea2643ec3163d4c837f21
Ziyad Duron <ZiyadDuron@andreas-mbp.lan>
2017-11-08 21:57:21 -0500

    added switch form button

---
+++ final-project/scripts/form-scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  $('#switch-button').click(function() {
+    if($('#form-header').text('Book Us!')) {
+      $('#form-header').text('Contact Us!')
+    }
+  })
+})
