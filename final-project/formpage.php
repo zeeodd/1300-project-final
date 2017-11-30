0c3ec15745d3052ec6e522ea67aed584bc524843
Ziyad Duron <ZiyadDuron@andreas-mbp.lan>
2017-11-30 11:20:37 -0500

    changes for milestone 4

--- final-project/formpage.php
+++ final-project/formpage.php
@@ -6,6 +6,13 @@ $date = $_SESSION['date'];
 $timeBefore = $_SESSION['time-before'];
 $timeAfter = $_SESSION['time-after'];
 $note = $_SESSION['note'];
+$state = $_SESSION['state'];
+
+if ($state == 1) {
+  $date = 'N/A';
+  $timeBefore = 'N/A';
+  $timeAfter = 'N/A';
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,21 +24,34 @@ $note = $_SESSION['note'];
 </head>
 
 <body>
-  <h2>
-    Form page still under construction!
-  </h2>
+  <h1><a id="navTitle" href="index.php">Original Cornell Syncopators</a></h1>
+  <?php include("includes/navigation.php"); ?>
 
-  <div>
-    <?php echo( htmlspecialchars($name) ); ?> <br/>
-    <?php echo( htmlspecialchars($email) ); ?> <br/>
-    <?php echo( htmlspecialchars($date) ); ?> <br/>
-    <?php echo( htmlspecialchars($timeBefore) ); ?> <br/>
-    <?php echo( htmlspecialchars($timeAfter) ); ?> <br/>
-    <?php echo( htmlspecialchars($note) ); ?> <br/>
-  </div>
+  <p id="formpage-header">
+    Thank you for submitting the following information! We will respond to you
+    as quickly as possible. In the meantime, feel free to explore the rest of our
+    website and listen to our music!
+  </p>
 
-  <div>
-    <a href="index.php">Return Home</a>
+  <div id=#form-container>
+    <table id="form-table">
+      <tr>
+        <th class="th">Name</th>
+        <th class="th">Email</th>
+        <th class="th">Date</th>
+        <th class="th">Start Time</th>
+        <th class="th">End Time</th>
+        <th class="th">Message</th>
+      </tr>
+      <tr>
+        <td class="td"> <?php echo( htmlspecialchars($name) ); ?> </td>
+        <td class="td"> <?php echo( htmlspecialchars($email) ); ?> </td>
+        <td class="td"> <?php echo( htmlspecialchars($date) ); ?> </td>
+        <td class="td"> <?php echo( htmlspecialchars($timeBefore) ); ?> </td>
+        <td class="td"> <?php echo( htmlspecialchars($timeAfter) ); ?> </td>
+        <td class="td"> <?php echo( htmlspecialchars($note) );?> </td>
+      </tr>
+    </table>
   </div>
 </body>
 
