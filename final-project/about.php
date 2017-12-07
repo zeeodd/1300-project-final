c13bbd160c9ceffd7962afa40907eeed08dd2abc
Noah Beller <noahbeller@dhcp-rhodes-2354.eduroam.cornell.edu>
2017-12-07 13:43:25 -0500

    transparent backgrounds

--- final-project/about.php
+++ final-project/about.php
@@ -15,61 +15,115 @@
   <section id="tiles">
 
     <div class="tile">
-      <a href="#noah"> Noah Li </a>
+      <a href="#noah_header"> Noah Li </a>
     </div>
 
     <div class="tile">
-      <a href="#rishi"> Rishi Verma </a>
+      <a href="#rishi_header"> Rishi Verma </a>
     </div>
 
     <div class="tile">
-      <a href="#amit"> Amit Mizrahi </a>
+      <a href="#amit_header"> Amit Mizrahi </a>
     </div>
 
     <div class="tile">
-      <a href="#nikki"> Nikki Love </a>
+      <a href="#nikki_header"> Nikki Love </a>
     </div>
 
+    <div class="tile">
+      <a href="#colin_header"> Colin Hancock </a>
+    </div>
+
+    <div class="tile">
+      <a href="#troy_header"> Troy Anderson </a>
+    </div>
+
+    <div class="tile">
+      <a href="#robbert_header"> Robbert Van Renesse </a>
+    </div>
   </section>
 
-  <section class='blurb'>
+  <section id="noah_header">
     <h2>Noah Li</h2>
     <div class="about">
-      <img src="images/noah-li.png" alt="Noah Li playing" class="profile" id="noah">
-      <p>Noah Li is a 3rd year information science major at Cornell University. He plays the drums good.</p>
-      <img src="images/c-drums.jpeg" alt="Noah's instrument" class="instrument" id="c-drums">
+      <img src="images/Noah_Li.jpg" alt="Noah Li playing" class="profile" id="noah">
+      <p class= "paragraph">Noah Li is an Information Science major from Westchester, New York. Before joining the Syncopators, Noah spent his time playing Blink-182 in his basement, which he is arguably still doing. Noah was classically trained as a pianist, violist, and conductor. He will sing if the price is high enough, and his family is not present. Major drumming influences include: Travis Barker, Hal Smith, Tony Sbarbaro, and Ray Bauduc.</p>
+      <img src="images/c-drums.png" alt="Noah's instrument" class="instrument" id="c-drums">
       <!--Source: http://www.clker.com/clipart-4464.html-->
     </div>
   </section>
 
-  <section class='blurb'>
+  <div id="rishi_header">
+  </div>
+  <section>
     <h2>Rishi Verma</h2>
     <div class="about">
       <img src="images/trombone.jpg" alt="Rishi Verma playing" class="profile" id="rishi">
-      <p>Rishi Verma is some dude. Lorem ipsum dolor sit amet, consectetur adipisicing elit. He plays the trombone.</p>
-      <img src="images/c-trombone.jpg" alt="Rishi's instrument" class="instrument" id="c-trombone">
+      <p class= "paragraph">Rishi Verma is some dude. Lorem ipsum dolor sit amet, consectetur adipisicing elit. He plays the trombone.</p>
+      <img src="images/c-trombone.png" alt="Rishi's instrument" class="instrument" id="c-trombone">
       <!--Source: https://www.123rf.com/photo_54963817_stock-illustration-2d-cartoon-illustration-of-bass-trombone.html-->
     </div>
   </section>
 
+  <div id="amit_header">
+  </div>
   <section class='blurb'>
     <h2>Amit Mazrahi</h2>
     <div class="about">
       <img src="images/piano.jpg" alt="Amit Mizrahi playing" class="profile" id="amit">
-      <p> Amit Mazrahi Lorem ipsum dolor sit amet, consectetur adipisicing elit. He plays the piano.</p>
+      <p class= "paragraph"> Amit Mizrahi, from West Bloomfield, Michigan, is a computer science major in the College of Engineering. He has been studying classical piano since the age of seven and jazz piano since high school. On campus, he is involved with the Cornell Data Science project team and co-founded a student-taught introductory data science course. He also writes software as a research assistant at the Johnson Business School. Amit will be graduating and moving to Silicon Valley in 2018 to work as a software engineer.</p>
       <img src="images/c-piano.png" alt="Amit's instrument" class="instrument" id="c-piano">
       <!-- Source: http://clipart-library.com/cartoon-piano-pictures.html -->
     </div>
   </section>
 
+  <div id="nikki_header">
+  </div>
   <section class='blurb'>
     <h2>Nikki Love</h2>
     <div class="about">
       <img src="images/violin.jpg" alt="Nikki Love playing" class="profile" id="nikki">
-      <p> Nikki loveLorem ipsum dolor sit amet, consectetur adipisicing elit. She plays the violin.</p>
+      <p class= "paragraph"> Nikki loveLorem ipsum dolor sit amet, consectetur adipisicing elit. She plays the violin.</p>
       <img src="images/c-violin.png" alt="Nikki's instrument" class="instrument">
-      <!--Source: http://www.clker.com/clipart-violin.html -->
+            <!--Source: http://www.clker.com/clipart-violin.html -->
     </div>
   </section>
+
+  <div id="colin_header">
+  </div>
+  <section>
+    <h2>Colin Hancock</h2>
+    <div class="about">
+      <img src="images/muted_trumpet.jpg" alt="Colin Hancock playing" class="profile" id="colin">
+      <p class= "paragraph">Colin Hancock is a junior Urban and Regional Studies major from Buda, Texas. He is also a music and law and society minor. Colin enjoys playing and listening to early jazz, as well as the study of its history and influence. He picked up the trumpet after falling in love with the music of Bix Beiderbecke, Louis Armstrong and Sidney Bechet. He is also an avid record collector and amateur historian in territory and Texas Jazz. He plans on eventually applying to law schools, as well as playing music professionally for his career.</p>
+      <img src="images/c-trumpet.gif" alt="Colin's instrument" class="instrument" id="c-trumpet">
+      <!--Source: https://thetomatos.com/free-clipart-29687/-->
+    </div>
+  </section>
+
+  <div id="troy_header">
+  </div>
+  <section>
+    <h2>Troy Anderson</h2>
+    <div class="about">
+      <img src="images/man_sax.jpg" alt="Troy Anderson playing" class="profile" id="troy">
+      <p class= "paragraph">Troy Anderson is an Undergraduate from Pittsford, NY,  studying Biological Sciences set to graduate in 2019. His passion for jazz began with a Pre-Collegiate Diploma in Jazz Saxophone from Eastman Community Music School, and has carried over to playing in various groups on Cornell’s campus. Beyond jazz, he eventually hopes to pursue a PhD in Genetics.</p>
+      <img src="images/c-sax.png" alt="Troy's instrument" class="instrument" id="c-sax">
+      <!--Source: https://www.zazzle.com/saxophone+cartoon+postcards-->
+    </div>
+  </section>
+
+  <div id="robbert_header">
+  </div>
+  <section>
+    <h2>Robbert Van Renesse</h2>
+    <div class="about">
+      <img src="images/banjo.jpg" alt="Robbert Van Renesse playing" class="profile" id="troy">
+      <p class= "paragraph">Robbert van Renesse lives in Ithaca, NY and loves playing early jazz on tenor and plectrum banjo as well as uke and guitar.  Besides the Original Cornell Syncopators, Robbert plays with the Ithaca-based Ageless Jazz Band and the Cortland-based JazzHappensBand.</p>
+      <img src="images/c-banjo.png" alt="Robbert's instrument" class="instrument" id="c-banjo">
+      <!--Source: https://www.clker.com/clipart-12745.html-->
+    </div>
+  </section>
+
 </body>
 </html>
